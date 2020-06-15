7bd8b1012e845e77a870771e5c6b5680e57d249e
balraj-johal <balraj-johal@users.noreply.github.com>
2020-06-15 21:13:01 +0100

    exclude current post from relateds

--- single-track.php
+++ single-track.php
@@ -34,7 +34,6 @@
       sorted randomly
     -->
     <?php 
-      $post_id - get_the_ID();
       //get tags as comma seperated string, get_the_tags_list() had dumb formatting on it
       //that stopped it from being quieried
       $term_list = join(',', array_map(function($t) { return $t->name; }, get_the_tags()));
@@ -42,16 +41,16 @@
         'category_name' => 'track',
         'posts_per_page' => 4,
         'post_status' => 'publish',
-        'post__not_in' => array( $post_id ),
+        'post__not_in' => array(get_the_ID()),
         'orderby' => 'rand',
         'tag' => $term_list
       );
       $related = new WP_Query( $related_args );
       if ( $related->have_posts() ) : ?>
 
-        <div class="container">
+        <div class="container-override">
           <h1 class="block-title">other shit you might like</h1>
-          <div class="row tracks-all">
+          <div class="row tracks-all content">
             <?php while ( $related->have_posts() ) : $related->the_post(); ?>
               <div class="col-6 col-lg-4">
                 <?php get_template_part('track-card'); ?>
