9302aeae541acbcd22c250b48dc0d30eaa2fcf69
balraj-johal <balraj-johal@users.noreply.github.com>
2020-06-15 21:09:57 +0100

    add tag related posts!! fix footer also

--- footer.php
+++ footer.php
@@ -1,6 +1,6 @@
     <?php wp_footer(); ?> <!-- "hook" for plugin code !-->
     
-    <div class="footer">Site by Balraj Johal - <a href="http://www.balraj.cool">www.balraj.cool</a></div>
+    <div class="footer"><a class="fix-a" href="http://www.balraj.cool">Site by Balraj Johal</a></div>
 
     <script type="text/javascript" src="//code.jquery.com/jquery-1.11.0.min.js"></script>
     <script type="text/javascript" src="//code.jquery.com/jquery-migrate-1.2.1.min.js"></script>
--- single-track.php
+++ single-track.php
@@ -11,6 +11,9 @@
     </div> -->
   </div>
 
+  <!-- 
+    Track details, i.e. title and description
+  -->
   <div class="container-override">
     <?php while(have_posts()) : the_post(); ?>
       <!-- <div class="details">
@@ -25,26 +28,38 @@
       <br />
       <br />
     <?php endwhile; ?>
-
-    <?php
-      $randTracks = new WP_Query( array(
+    
+    <!-- 
+      Tracks related by tags, not including current post
+      sorted randomly
+    -->
+    <?php 
+      $post_id - get_the_ID();
+      //get tags as comma seperated string, get_the_tags_list() had dumb formatting on it
+      //that stopped it from being quieried
+      $term_list = join(',', array_map(function($t) { return $t->name; }, get_the_tags()));
+      $related_args = array(
         'category_name' => 'track',
-        'orderby'   => 'rand',
-        'posts_per_page' => 3,
-      )); 
-    ?>
+        'posts_per_page' => 4,
+        'post_status' => 'publish',
+        'post__not_in' => array( $post_id ),
+        'orderby' => 'rand',
+        'tag' => $term_list
+      );
+      $related = new WP_Query( $related_args );
+      if ( $related->have_posts() ) : ?>
 
-    <?php if ( $randTracks->have_posts() ) : ?>
-      <div class="container">
-        <h1 class="block-title">other shit you might like</h1>
-        <div class="row tracks-all">
-          <?php while ( $randTracks->have_posts() ) : $randTracks->the_post(); ?>
-            <div class="col-6 col-lg-4">
-              <?php get_template_part('track-card'); ?>
-            </div>
-          <?php endwhile; ?>
+        <div class="container">
+          <h1 class="block-title">other shit you might like</h1>
+          <div class="row tracks-all">
+            <?php while ( $related->have_posts() ) : $related->the_post(); ?>
+              <div class="col-6 col-lg-4">
+                <?php get_template_part('track-card'); ?>
+              </div>
+            <?php endwhile; ?>
+          </div>
         </div>
-      </div>
+
       <?php wp_reset_postdata(); ?>
     <?php else : ?>
       <p>No tracks!!</p>
